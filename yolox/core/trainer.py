0bedbf8be2f385331271457892ce8b9eeadb1201
Rds <553925448@qq.com>
2022-02-09 10:35:23 +0800

    add(trainer)：an unfinished early version

--- yolox/core/trainer.py
+++ yolox/core/trainer.py
@@ -1,3 +1,111 @@
 import pytorch_lightning as pl
 from pytorch_lightning.trainer.trainer import Trainer
+from loguru import logger
+from typing import Dict
+
+
+class LoggerCallback(pl.Callback):
+    def __init__(self, exp: Dict, args: Dict):
+        """
+        :param exp:
+        :param args:
+        """
+        super(LoggerCallback, self).__init__()
+        self.exp = exp
+        self.args = args
+    def on_train_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+        logger.info("args: {}".format(self.args))
+        logger.info("exp value:\n{}".format(self.exp))un
+
+    def on_train_epoch_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+
+
+class LightningModule(pl.Callback):
+
+
+class LightningDataModule(pl.Callback):
+    def __init__(self):
+
+    def prepare_data(self):
+        pass
+
+    def setup(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", stage: Optional[str] = None) -> None:
+        from yolox.data import (
+            COCODataset,
+            TrainTransform,
+            YoloBatchSampler,
+            DataLoader,
+            InfiniteSampler,
+            MosaicDetection,
+            worker_init_reset_seed,
+        )
+        from yolox.utils import (
+            wait_for_the_master,
+            get_local_rank,
+        )
+
+        local_rank = get_local_rank()
+
+        with wait_for_the_master(local_rank):
+            dataset = COCODataset(
+                data_dir=self.data_dir,
+                json_file=self.train_ann,
+                img_size=self.input_size,
+                preproc=TrainTransform(
+                    max_labels=50,
+                    flip_prob=self.flip_prob,
+                    hsv_prob=self.hsv_prob),
+                cache=cache_img,
+            )
+
+        dataset = MosaicDetection(
+            dataset,
+            mosaic=not no_aug,
+            img_size=self.input_size,
+            preproc=TrainTransform(
+                max_labels=120,
+                flip_prob=self.flip_prob,
+                hsv_prob=self.hsv_prob),
+            degrees=self.degrees,
+            translate=self.translate,
+            mosaic_scale=self.mosaic_scale,
+            mixup_scale=self.mixup_scale,
+            shear=self.shear,
+            enable_mixup=self.enable_mixup,
+            mosaic_prob=self.mosaic_prob,
+            mixup_prob=self.mixup_prob,
+        )
+
+        self.dataset = dataset
+
+        if is_distributed:
+            batch_size = batch_size // dist.get_world_size()
+
+        sampler = InfiniteSampler(len(self.dataset), seed=self.seed if self.seed else 0)
+
+        batch_sampler = YoloBatchSampler(
+            sampler=sampler,
+            batch_size=batch_size,
+            drop_last=False,
+            mosaic=not no_aug,
+        )
+
+        dataloader_kwargs = {"num_workers": self.data_num_workers, "pin_memory": True}
+        dataloader_kwargs["batch_sampler"] = batch_sampler
+
+        # Make sure each process has different random seed, especially for 'fork' method.
+        # Check https://github.com/pytorch/pytorch/issues/63311 for more details.
+        dataloader_kwargs["worker_init_fn"] = worker_init_reset_seed
+
+def build_trainer(exp, args):
+    # TODO missing attribute/function: rank, local_rank, device, ema, save_history_ckpt
+    trainer = Trainer(max_epochs=exp.max_epoch,
+                      amp_backend=args.amp_backend,
+                      precision=args.precision,
+                      accelerator=args.acc
+                      )
+
+
+
+
 
